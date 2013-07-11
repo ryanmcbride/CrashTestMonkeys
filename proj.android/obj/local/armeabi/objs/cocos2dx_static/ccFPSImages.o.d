957961633e21cf32844c9a6891fd62c77c1dacb2
Admin <administrator@Ryans-iMac-2.local>
2013-07-10 20:53:44 -0600

    removed some files

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Users/administrator/Desktop/depot/CrashTestMonkeys/proj.android/../libs//cocos2dx/ccFPSImages.c \
- /Users/administrator/Desktop/depot/CrashTestMonkeys/proj.android/../libs//cocos2dx/ccFPSImages.h
-
-/Users/administrator/Desktop/depot/CrashTestMonkeys/proj.android/../libs//cocos2dx/ccFPSImages.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /Users/administrator/Desktop/depot/CrashTestMonkeys/proj.android/../libs//cocos2dx/kazmath/src/neon_matrix_impl.c \
- /Users/administrator/Desktop/depot/CrashTestMonkeys/proj.android/../libs//cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/Users/administrator/Desktop/depot/CrashTestMonkeys/proj.android/../libs//cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
